1621f7dcaf006519ba74448cd3be375bf33192d3
Nickop21 <ny8244@gmail.com>
2024-07-05 00:04:15 +0530

    initail setupd for next js shadcn tailwind css

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="bg-black flex min-h-screen flex-col items-center justify-between p-24">
+   <p className="text-white"></p>
+    </main>
+  );
+}
