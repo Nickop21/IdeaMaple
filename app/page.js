de21957a9e4a69e1fd6f460f9bc7b3201606e688
Nickop21 <ny8244@gmail.com>
2024-07-06 02:22:53 +0530

    packaged json changes

--- app/page.js
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <main className="bg-black flex min-h-screen flex-col items-center justify-between p-24">
-   <p className="text-white"></p>
-    </main>
-  );
-}
