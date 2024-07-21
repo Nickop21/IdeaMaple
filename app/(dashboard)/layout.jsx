fec4fc7548de0634d9e609f25752608b43d3d7ff
Nickop21 <ny8244@gmail.com>
2024-07-21 17:24:10 +0530

    dashbord page and layout added

--- app/(dashboard)/layout.jsx
+++ app/(dashboard)/layout.jsx
@@ -7,10 +7,10 @@ function Dashboardlayout({ children }) {
   return (
     <main className="h-full">
       <Sidebar />
-      <div className="pl-[60px] text-white">
+      <div className="pl-[60px]">
         <div className="flex gap-x-3 h-full">
             <OrgSidebar/>
-          <div className="h-full flex-1 bg-slate-400">
+          <div className="h-full flex-1 bg-black">
            <Navbar/>
             {children}
             </div>
